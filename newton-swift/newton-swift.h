5d628d99581f86e1443c40197a209701cc4f34c6
Tyler Walker <tyjohnwalker@gmail.com>
2017-08-03 21:32:47 -0700

    Initial Commit

---
+++ newton-swift/newton-swift.h
@@ -0,0 +1,19 @@
+//
+//  newton-swift.h
+//  newton-swift
+//
+//  Created by Tyler Walker on 8/3/17.
+//  Copyright © 2017 Tyler Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for newton-swift.
+FOUNDATION_EXPORT double newton_swiftVersionNumber;
+
+//! Project version string for newton-swift.
+FOUNDATION_EXPORT const unsigned char newton_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newton_swift/PublicHeader.h>
+
+
